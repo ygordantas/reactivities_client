a716371f3ac4cafb9817fa3168513f8ffb8a716d
Ygor Furtado <yfurtado@obsglobal.com>
2021-02-24 10:56:02 -0600

    added client ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { List, Header } from 'semantic-ui-react';
 
 function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:44372/api/Activities").then(res =>{
+      setActivities(res.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header as="h2" icon='users' content="Reactivities" />
+        <List>
+          {activities.map((act: any) => (
+            <List.Item key={act.id}>{act.description}</List.Item>
+          ))}
+        </List>
     </div>
   );
 }
